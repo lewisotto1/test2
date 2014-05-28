e4b8d37630808afce7c514b60160a6a7dbb5d56b
lewisotto1 <lewis@otto1.net>
2014-05-29 09:33:35 +1000

    y

--- file1.py
+++ file1.py
@@ -22,17 +22,20 @@ if my_in_type == 'rsync':
     my_in_ip = input('ip, user and directory to import from: ')
     call(["rsync", "-azv", my_in_ip, myRoot])
 elif my_in_type == 'git':
-    my_in_files = input('url: ')
+    my_in_files = input('git url: ')
     call(["git", "clone", my_in_files, myRoot])
 else:
     str('Invalid input, exiting')
     exit()
+asdf = my_in_files.rsplit('/')[4]
+asdf = asdf[:-4]
+myRoot2 = myRoot + asdf
 server_cfg = "server {\nlitsen "+my_in_port+"\nserver_name "+my_in_name+" www."+my_in_name+"\n\nlocation / "
-server_cfg += "{\n root "+myRoot+"\n Index   index.php index.html index.htm\n}\n\n"
+server_cfg += "{\n root "+myRoot2+"\n Index   index.php index.html index.htm\n}\n\n"
 if my_in_php == 'yes':
     server_cfg += "location ~ \.php$ {\n include /etc/nginx/fastcgi_params;\n"
     server_cfg += " fastcgi_pass unix:/tmp/php5-fpm.sock;\n fastcgi_index index.php\n"
-    server_cfg += " fastcgi_param SCRIPT_FILENAME "+myRoot+"$fast_cgi_scriptname;\n}\n"
+    server_cfg += " fastcgi_param SCRIPT_FILENAME "+myRoot2+"$fast_cgi_scriptname;\n}\n"
 
 server_cfg += "}\n"
 file1.write(server_cfg)
