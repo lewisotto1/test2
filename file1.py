c470a5ebd95c7afcc067d679333573087c4c0ad8
lewisotto1 <lewis@otto1.net>
2014-05-29 10:55:42 +1000

    y

--- file1.py
+++ file1.py
@@ -2,14 +2,15 @@
 
 import os
 from subprocess import call
-my_in = "/etc/nginx/sites-available"
+my_in = "/etc/nginx/sites-available/"
 my_in_name = input('enter server name: ')
+print(my_in+my_in_name)
 try:
-    file1 = open(my_in+my_in_name, 'r+')
+    file1 = open(my_in+my_in_name, 'w+')
 except IOError:
-    str('cannot open file.')
+    print(str('cannot open file.'))
     exit()
-s = 'file'+repr(my_in)+'opened'
+s = 'file'+repr(my_in+my_in_name)+'opened'
 print(s)
 my_in_port = "80"
 myRoot = "/usr/share/nginx/html/"
@@ -17,6 +18,7 @@ myRoot += my_in_name
 my_in_php = input('do you want php? [yes/no]: ')
 if not os.path.exists(myRoot):
     os.makedirs(myRoot)
+    os.makedirs(myRoot+"/logs")
 my_in_type = input('import files form github or rsync? [rsync/git]: ')
 if my_in_type == 'rsync':
     my_in_ip = input('ip, user and directory to import from: ')
@@ -24,17 +26,18 @@ if my_in_type == 'rsync':
 elif my_in_type == 'git':
     my_in_files = input('git url: ')
     call(["git", "clone", my_in_files, myRoot])
+    asdf = my_in_files.rsplit('/')[4]
+    asdf = asdf[:-4]
+    myRoot += asdf
 else:
     str('Invalid input, continuing...')
-asdf = my_in_files.rsplit('/')[4]
-asdf = asdf[:-4]
-myRoot2 = myRoot + asdf
 server_cfg = "server {\nlitsen "+my_in_port+"\nserver_name "+my_in_name+" www."+my_in_name+"\n\nlocation / "
-server_cfg += "{\n root "+myRoot2+"\n Index   index.php index.html index.htm\n}\n\n"
+server_cfg += "{\n root "+myRoot+"\n Index   index.php index.html index.htm\n}\n\n"
 if my_in_php == 'yes':
     server_cfg += "location ~ \.php$ {\n include /etc/nginx/fastcgi_params;\n"
     server_cfg += " fastcgi_pass unix:/tmp/php5-fpm.sock;\n fastcgi_index index.php\n"
-    server_cfg += " fastcgi_param SCRIPT_FILENAME "+myRoot2+"$fast_cgi_scriptname;\n}\n"
+    server_cfg += " fastcgi_param SCRIPT_FILENAME "+myRoot+"$fast_cgi_scriptname;\n}\n"
+server_cfg += "location ~ /\. {\n  access_log off;\n  log_not_found off;\n  deny all;\n"
 
 server_cfg += "}\n"
 file1.write(server_cfg)
