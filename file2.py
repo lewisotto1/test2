432f254b19df5f8881fa5f179e26a52d5be8729b
lewisotto1 <lewis@otto1.net>
2014-06-13 15:26:54 +1000

    y

--- file2.py
+++ file2.py
@@ -16,10 +16,12 @@ parser = argparse.ArgumentParser(description="Nginx server block creator")
 parser.add_argument('server', help="Server name")
 parser.add_argument('-g', "--git", help="Git url for files")
 parser.add_argument('-r', "--rsync", help="Rsync for files")
+parser.add_argument('-q', "--port", help="alternate port for rsync")
 parser.add_argument('-p', "--php", action='store_true', help="Include php block")
 parser.add_argument('-s', "--ssl", action='store_true', help="make block SSL")
-parser.add_argument('-l', "--ls", action='store_true', help="link files to enabled")
+parser.add_argument('-l', "--ln", action='store_true', help="link files to enabled")
 args = parser.parse_args()
+
 try:
     file1 = open("/etc/nginx/sites-available/"+args.server, 'w+')
 except IOError:
@@ -27,35 +29,40 @@ except IOError:
     exit(1)
 myRoot = "/usr/share/nginx/html/"
 myRoot += args.server
-if args.rsync:
-    my_in_ip = input('ip, user and directory to import from: ')
-    call(["rsync", "-azv", my_in_ip, myRoot])
-elif args.git:
-    my_in_files = input('git url: ')
-    call(["git", "clone", my_in_files, myRoot])
-    call(["chmod", "600", myRoot+".git"])
-else:
-    print(str('no files selected, continuing...'))
+myRoot2 = myRoot+"/public_html"
+
 if not os.path.exists(myRoot):
     os.makedirs(myRoot)
+    os.makedirs(myRoot2)
     os.makedirs(myRoot+"/logs")
-if args.ssl:
-    server_cfg = "server {{\nlisten 443 ssl;\nserver_name {} www.{};\n\nlocation / {".format(args.server, args.server)
-    server_cfg += "\n  ssl_certificate {}.crt;\n ssl_certificate_key {}.key;".format(args.server, args.server)
+
+if args.rsync:
+    if args.port:
+        call(["rsync", "-e", "'ssh -p"+args.port+"'", args.rsync, myRoot2])
+    call(["rsync", "-azv", args.rsync, myRoot2])
+elif args.git:
+    call(["git", "clone", args.git, myRoot2])
 else:
-    server_cfg = "server {{\nlisten 80;\nserver_name {} www.{}\n\nlocation /".format(args.server, args.server)
-server_cfg += "\n root {}\n Index   index.php index.html index.htm\n}}\n\n".format(myRoot)
+    print(str('no files selected, continuing...'))
+
+if args.ssl:
+    server_cfg = "server {{\nlisten 443 ssl;\nserver_name {0} www.{0};\n\nlocation / {{".format(args.server)
+    server_cfg += "\n  ssl_certificate {0}.crt;\n ssl_certificate_key {0}.key;".format(args.server)
+else:
+    server_cfg = "server {{\nlisten 80;\nserver_name {0} www.{0}\n\nlocation / {{".format(args.server)
+server_cfg += "\n root {}\n Index   index.php index.html index.htm\n}}\n\n".format(myRoot2)
 if args.php:
     server_cfg += "location ~ \.php$ {\n include /etc/nginx/fastcgi_params;\n"
     server_cfg += " fastcgi_pass 127.0.0.1:9000;\n fastcgi_index index.php;\n"
     #server_cfg += " fastcgi_pass unix:/tmp/php5-fpm.sock;\n fastcgi_index index.php;\n"
-    server_cfg += " fastcgi_param SCRIPT_FILENAME {}$fast_cgi_scriptname;\n}}\n".format(myRoot)
+    server_cfg += " fastcgi_param SCRIPT_FILENAME {}$fast_cgi_scriptname;\n}}\n".format(myRoot2)
 server_cfg += "location ~ /\. {\n  access_log off;\n  log_not_found off;\n  deny all;\n}\n}"
 file1.write(server_cfg)
 file1.close()
+
 if args.ls:
     call(["ln", "-s", "/etc/nginx/sites-available/"+args.server, "/etc/nginx/sites-enabled/"+args.server])
-call(["chmod", "755", "-R", myRoot])
+call(["chown", "apache:apache", "-R", myRoot2])
 call(["service", "nginx", "reload"])
 print(str('exiting'))
 exit()
